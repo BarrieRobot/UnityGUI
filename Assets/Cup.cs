71c50d91989ecaaa1b3e12193389a1b659aead60
jonathanraes <jonathanraes@gmail.com>
2017-10-25 16:54:04 +0200

    added touchscript for TUIO controls

---
+++ Assets/Cup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class Cup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void gestureStateChangedHandler(object sender, GestureStateChangeEventArgs e) {
+		Debug.Log ("Gesture changed");
+	}
+	public void gest() {
+		Debug.Log ("tap detected");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
